0fa3adc84b5e0ab753ba54e8fee26d56dc0216bc
Fuzzy Yeti <fuzzyyeti@github.com>
2022-02-20 22:53:01 -0700

    add route to add coins after game win

--- src/api/routes/state.py
+++ src/api/routes/state.py
@@ -1,6 +1,4 @@
-from crypt import methods
 from bson.json_util import dumps
-from urllib import response
 from flask import Blueprint, jsonify, request, Response, session
 from api.database import mongodriver
 from api.utils.attribute_finder import get_nft_info
@@ -30,12 +28,25 @@ def upgrade_tank():
     if(not mint):
         return jsonify("Mint number not set"), 501
     if(not tank):
-        return jsonify("Need to set an item in the json data"), 401        
+        return jsonify("Need to set a tank in the json data"), 401        
     if api.game_logic.upgrade_tank(mint, tank):
         return jsonify("success"), 200
     else:
         return jsonify("fail"), 200
 
+
+@bp.route('/wingame', methods = ['POST'])
+def win_game():
+    mint = request.json['mint']
+    coins = request.json['coins']
+    if(not mint):
+        return jsonify("Mint number not set"), 501
+    if(not coins):
+        return jsonify("Need to set the number of coins json data"), 401
+    balance = db.get_coin_balance(mint)
+    db.set_coin_balance(mint, balance + coins)   
+    return jsonify("success"), 200
+    
 @bp.route('/buyitem', methods = ['POST'])
 def buy_item():
     mint = request.json['mint']
